41361b34eb1b32b0ae2f9549fc176a16a3da4e04
Kherman <herman@dev.vtb.ru>
2021-10-04 17:45:49 +0300

    Add action bar

--- app/src/main/java/com/alexxer/simplepostapplication/presentation/ui/Posts.kt
+++ app/src/main/java/com/alexxer/simplepostapplication/presentation/ui/Posts.kt
@@ -1,8 +1,6 @@
 package com.alexxer.simplepostapplication.presentation.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.alexxer.simplepostapplication.R
